2ec740f8cb0a6ae818a114b88744e49e801010e7
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-03-10 12:17:33 -0400

    chore: remove log

--- src/utils/getUserRole.js
+++ src/utils/getUserRole.js
@@ -1,20 +1,19 @@
 const jwt = require("jsonwebtoken");
 function getRolefromToken(AuthToken) {
-  if (!AuthToken) return null
+  if (!AuthToken) return null;
   const token = AuthToken.split(" ")[1];
   const decoded = jwt.decode(token);
-  console.log(decoded)
   return decoded.user.role;
 }
 function getUserIdFromToken(AuthToken) {
-  if (!AuthToken) return null
+  if (!AuthToken) return null;
   const token = AuthToken.split(" ")[1];
   const decoded = jwt.decode(token);
-  console.log(decoded)
+  console.log(decoded);
   return decoded.user._id;
 }
 
 module.exports = {
   getRolefromToken,
-  getUserIdFromToken
+  getUserIdFromToken,
 };
